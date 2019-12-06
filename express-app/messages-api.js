1e56d39c9530d1eadd0e68c88b7ee45b19d5e2dc
tomekgora <tomekgora@gmail.com>
2019-12-07 00:30:08 +0100

    middleware for call limit implemented, may need changes

--- express-app/messages-api.js
+++ express-app/messages-api.js
@@ -6,17 +6,25 @@ let reqCount = 0
 
 
 //Middelware
+
+const limitCallsMiddleware = (req, res, next) => {
+    reqCount++
+    if(reqCount > 5){
+        res.status(429).end()
+    }else{
+        next()
+    }
+};
+
 app.use(bodyParser.json())
+app.use(limitCallsMiddleware)
 
 //POST /messages endpoint
 app.post('/messages', (req, res) => {
-    reqCount++
     if(!req.body.text || req.body.text === '') {
         res.status(400).end()
-    }if(reqCount > 5){
-        res.status(429).end()
     }else{
-        console.log("The value of reqCount", reqCount)
+        console.log("The value of reqCount is:", reqCount)
         console.log("This is the post req.body.text:", req.body.text)
         res.json({
             message: "Message received loud and clear"
