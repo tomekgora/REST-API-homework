17ed75f5519c2276711e9afcd183d4eb53e50d41
tomekgora <tomekgora@gmail.com>
2019-12-07 00:16:50 +0100

    ugly implementation, 429 after 5 api calls works with let reqCount

--- express-app/messages-api.js
+++ express-app/messages-api.js
@@ -2,26 +2,26 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+let reqCount = 0
 
 
 //Middelware
 app.use(bodyParser.json())
 
-//POST endpoint
+//POST /messages endpoint
 app.post('/messages', (req, res) => {
-    let reqCount = 0
+    reqCount++
     if(!req.body.text || req.body.text === '') {
         res.status(400).end()
     }if(reqCount > 5){
         res.status(429).end()
     }else{
-        reqCount +=
+        console.log("The value of reqCount", reqCount)
         console.log("This is the post req.body.text:", req.body.text)
         res.json({
             message: "Message received loud and clear"
         })
     }
-
 });
 
 app.listen(port, () =>
