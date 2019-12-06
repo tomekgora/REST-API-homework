6d88d04b4e4df1395c3727c9a8ea1bd059d720c4
tomekgora <tomekgora@gmail.com>
2019-12-06 23:02:20 +0100

    using var reqCount won't work obv

--- express-app/messages-api.js
+++ express-app/messages-api.js
@@ -3,15 +3,25 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+
 //Middelware
 app.use(bodyParser.json())
 
 //POST endpoint
 app.post('/messages', (req, res) => {
-    console.log("This is the post req.body.text:", req.body.text)
-    res.json({
-        message: "Message received loud and clear"
-    })
+    let reqCount = 0
+    if(!req.body.text || req.body.text === '') {
+        res.status(400).end()
+    }if(reqCount > 5){
+        res.status(429).end()
+    }else{
+        reqCount +=
+        console.log("This is the post req.body.text:", req.body.text)
+        res.json({
+            message: "Message received loud and clear"
+        })
+    }
+
 });
 
 app.listen(port, () =>
