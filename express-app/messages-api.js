b0a2d970e0b9a0d6e3caa9bdda6113c168491804
tomekgora <tomekgora@gmail.com>
2019-12-06 21:59:13 +0100

    added body-parser and basic post endpoint

--- express-app/messages-api.js
+++ express-app/messages-api.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-app.post('/messages', (req, res, next) => {
-    console.log("This is the post req.body:", req.body)
+//Middelware
+app.use(bodyParser.json())
 
+//POST endpoint
+app.post('/messages', (req, res) => {
+    console.log("This is the post req.body:", req.body)
+    res.json({
+        message: "Message received loud and clear"
+    })
 });
-app.listen(port, () => console.log(`Messages App listening on port: ${port}`))
+
+app.listen(port, () =>
+    console.log(`Messages App listening on port: ${port}`)
+);
