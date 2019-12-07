91b9be4f0c2b35f76431e8a59bb5f96427614c56
tomekgora <tomekgora@gmail.com>
2019-12-07 20:15:48 +0100

    sequelize does not support type NUMBER, facepalm

--- movie/model.js
+++ movie/model.js
@@ -4,8 +4,12 @@ const db = require('../sequelize-rest')
 const Movie = db.define( 'movie', 
     {
         title: Sequelize.TEXT,
-        yearofRelease: Sequelize.INTEGER,
+        yearofRelease: Sequelize.NUMBER,
         synopsis: Sequelize.TEXT
     });
 
 module.exports = Movie
+
+        // title: { Sequelize.TEXT}
+        // yearOfRelease: Sequelize.NUMBER,
+        // synopsis: Sequelize.TEXT
