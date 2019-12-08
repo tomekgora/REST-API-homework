b71be06bbbb9d39b557d47ee9e56705e68a6d234
tomekgora <tomekgora@gmail.com>
2019-12-08 01:30:12 +0100

    trying to use .finally() and callback

--- sequelize-rest-api/sequelize-rest.js
+++ sequelize-rest-api/sequelize-rest.js
@@ -3,13 +3,20 @@ const Movie = require('./movie/model')
 const exampleData = require('./data/example-data')
 const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:hello@localhost:5432/postgres'
 
+function loadData() {
+    exampleData.map(movie => {
+        Movie.create(movie)
+    })
+};
 
 const db = new Sequelize(databaseUrl)
 
 db.sync()
-    .then(() => console.log("The database is up"))
+    .then((result) => console.log("The database is up", result))
     // .then(() => example.Data.map((movie) => Movie.create(movie)))
+    // thos ,ade np semse I need to use .finally()
     .catch(err => next(err))
+    .finally(loadData())
 
 // Movie.create({title:'titanic', yearOfRelease:1996, synopsis:'not bad'})
 //     .then(movie => {})
