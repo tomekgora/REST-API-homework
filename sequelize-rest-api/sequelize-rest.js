1852a11c31a2e1152c6e099b16f4b52fc65bb659
tomekgora <tomekgora@gmail.com>
2019-12-08 01:07:50 +0100

    trying to use the model create method inside a then()

--- sequelize-rest-api/sequelize-rest.js
+++ sequelize-rest-api/sequelize-rest.js
@@ -3,22 +3,16 @@ const Movie = require('./movie/model')
 const exampleData = require('./data/example-data')
 const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:hello@localhost:5432/postgres'
 
+
 const db = new Sequelize(databaseUrl)
 
 db.sync()
     .then(() => console.log("The database is up"))
-    .then(
-        () => exampleData.map(movie => {Movie.create(movie)})
-    )
+    .then(() => example.Data.map((movie) => Movie.create(movie)))
     .catch(err => next(err))
 
+// Movie.create({title:'titanic', yearOfRelease:1996, synopsis:'not bad'})
+//     .then(movie => {})
 
-// Movie.create
-//     .then(movie => res.send(movie))
-//     .catch(err => next(err))
-
-// Movie.create(
-//     { title='hello', yearOfRelease=1992, synopsis='not bad'}
-// )
 
 module.exports = db
